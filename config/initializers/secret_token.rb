b594d3ec2b1b076cf38479a401c8eb81d22f950a
robin miller & Liz Tom <student@epicodus.com>
2014-04-04 09:19:52 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StuntDatabase::Application.config.secret_key_base = 'd250caf84e46fc1448d25a9472084f292ea618dec1d1418e74c2b00cbe2f0b06912f9f5001ac6d0ea653e63874e2c1f771bf1204affb247aa0b38e56a61ed079'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StuntDatabase::Application.config.session_store :cookie_store, key: '_stunt_database_session'
